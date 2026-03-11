f62a88de9c2086528f4cf45e28f17f8ae6d56ede
caolin <q42602736@gmail.com>
2026-03-11 23:42:18 +0800

    fix: 调整 Linux AMD64 构建输出文件名

--- config-server/build-nexgen-client-linux-amd64.sh
+++ config-server/build-nexgen-client-linux-amd64.sh
@@ -7,7 +7,7 @@ cd "$SCRIPT_DIR"
 
 mkdir -p dist
 
-OUTPUT="dist/nexgen-client-linux-amd64"
+OUTPUT="dist/nexgen-client"
 
 env GOOS=linux GOARCH=amd64 go build -trimpath -ldflags='-s -w' -o "$OUTPUT" .
 
