a1c90df64b5a087935b3235bb1eab4a40e070f64
caolin <q42602736@gmail.com>
2026-05-10 02:24:22 +0800

    Revert "添加客户端日志解密脚本入口"
    This reverts commit 1dd8adf70b78ca5938a0c87fd307eaa3c4a1dd0b.

--- config-server/decrypt-nexgen-client-logs.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-set -eu
-
-CLIENT_DIR="/Users/caolin/Desktop/projects/Xboard-Mihomo_sub"
-SCRIPT_PATH="$0"
-
-DECRYPT_LOGS_COMMAND_NAME="$SCRIPT_PATH" exec "$CLIENT_DIR/decrypt_logs.sh" "$@"
